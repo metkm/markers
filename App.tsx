2ed31bd01a05946ed1607e63c651a25fa2fc0a4f
metkm <metinkorkmaz417@gmail.com>
2025-06-08 23:46:13 +0300

    init

--- App.tsx
+++ App.tsx
@@ -1,46 +1,53 @@
-import { memo, useMemo } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { memo, useMemo, useState } from "react";
+import { Button, StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
-const CustomMarker = memo(({ coordinate, id }: { coordinate: { latitude: number; longitude: number }; id: number }) => {
-  return (
-    <Marker key={id} coordinate={coordinate} tracksViewChanges={false}>
-      <View
-        style={{
-          width: 10,
-          height: 10,
-          backgroundColor: 'red',
-        }}
-      />
-    </Marker>
-  );
-});
+const markers = [...Array(50).keys()].map((item) => ({
+  id: item,
+  coordinate: {
+    latitude: 38 + item / 4,
+    longitude: 16,
+  },
+}));
+
+// const CustomMarker = memo(
+//   ({ coordinate, id }: { coordinate: { latitude: number; longitude: number }; id: number }) => {
+//     return (
+//       <Marker key={id} coordinate={coordinate} tracksViewChanges={false}>
+//         <View
+//           style={{
+//             width: 10,
+//             height: 10,
+//             backgroundColor: "red",
+//           }}
+//         />
+//       </Marker>
+//     );
+//   }
+// );
 
 export default function App() {
-    const markers = useMemo(() => {
-    return [...Array(50).keys()].map((item) => ({
-      id: item,
-      coordinate: {
-        latitude: 38 + item / 4,
-        longitude: 16,
-      },
-    }));
-  }, []);
+  const [showMarkers, setShowMarkers] = useState(false);
 
   return (
-    <MapView style={{ flex: 1 }}>
-      {markers.map((marker) => (
-        <CustomMarker key={marker.id} id={marker.id} coordinate={marker.coordinate} />
-      ))}
-    </MapView>
+    <>
+      <Button title="Toggle Markers" onPress={() => setShowMarkers((show) => !show)} />
+
+      <MapView style={{ flex: 1 }}>
+        {showMarkers && (
+          markers.map((marker) => (
+          <Marker key={marker.id} coordinate={marker.coordinate} tracksViewChanges={false}>
+            <View
+              style={{
+                width: 10,
+                height: 10,
+                backgroundColor: "red",
+              }}
+            />
+          </Marker>
+          ))
+        )}
+      </MapView>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
