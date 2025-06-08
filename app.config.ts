7ab934e7d7c5d154a9700379fdfb0da4cf3d2da8
metkm <metinkorkmaz417@gmail.com>
2025-06-09 00:18:11 +0300

    new arch, updated version

--- app.config.ts
+++ app.config.ts
@@ -7,8 +7,8 @@ const config: ExpoConfig = {
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
-  newArchEnabled: false,
-  // newArchEnabled: true,
+  // newArchEnabled: false,
+  newArchEnabled: true,
   splash: {
     image: "./assets/splash-icon.png",
     resizeMode: "contain",
